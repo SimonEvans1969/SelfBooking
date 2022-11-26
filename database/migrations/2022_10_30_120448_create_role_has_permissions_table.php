f63b92bb86362684ea887c78ae5bf7208f3c9bc2
SimonEvans1969 <simon@nautilus-consulting.co.uk>
2022-11-26 17:18:54 +0000

    Sort out migrations

---
+++ database/migrations/2022_10_30_120448_create_role_has_permissions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('role_has_permissions', function (Blueprint $table) {
+            $table->foreignId('permission_id')->references('id')->on('permissions');
+            $table->foreignId('role_id')->references('id')->on('roles');
+            $table->primary(['permission_id', 'role_id'], 'role_has_permissions_permission_id_role_id_primary');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('role_has_permissions');
+    }
+};
